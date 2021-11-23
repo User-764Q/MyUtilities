081ebf29fda4a4364fbfa25b03311483d7b0c1d7
User_746D <86132568+User-764Q@users.noreply.github.com>
2021-11-24 06:50:59 +1300

    Create CodeFromDataCamp.r

---
+++ CodeFromDataCamp.r
@@ -0,0 +1,6 @@
+# this file is to store some code I might want to use again that I learned from datacamp 
+
+# mean of a binary 
+
+# Analysing a bunch of different models with adjusted p values nest and un-nest. 
+

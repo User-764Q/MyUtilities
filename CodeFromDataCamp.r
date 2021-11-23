50f614d5eabfdcb234d6112e279bd6cfb6fec433
User_746D <86132568+User-764Q@users.noreply.github.com>
2021-11-24 06:56:21 +1300

    Update CodeFromDataCamp.r

--- CodeFromDataCamp.r
+++ CodeFromDataCamp.r
@@ -2,5 +2,31 @@
 
 # mean of a binary 
 
+# Start with by_year_country dataset
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+
 # Analysing a bunch of different models with adjusted p values nest and un-nest. 
+
+# Add one more step that unnests the tidied column
+country_coefficients <- by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)),
+         tidied = map(model, tidy)) %>%
+         unnest(tidied)
+# Filter for only the slope terms
+slope_terms <- country_coefficients %>%
+  filter(term == "year")
+
+# Add p.adjusted column, then filter
+slope_terms %>%
+mutate(p.adjusted = p.adjust(p.value)) %>%
+  filter(p.adjusted < .05)
+
+# Print the resulting country_coefficients variable
+country_coefficients
+
 
